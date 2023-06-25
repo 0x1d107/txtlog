40ff338049d95decf2eab35edd028a8bb7fd6ea8
0x1d107 <yablinchik@gmail.com>
2023-06-25 15:02:19 +0500

    nexus

--- static/scripts/nexus.sh
+++ static/scripts/nexus.sh
@@ -4,11 +4,21 @@ while [ -n "$LINK" ]
 do
     HOST=${URL%%/*}
     QUERY=${URL#*/}
-    PAGE=$(mktemp nexpage.XXXX.txt)
+    PAGE=$(mktemp nexpage.XXXX)
     echo /$QUERY |nc $HOST 1900 > $PAGE
     clear
     echo "=> nex://$URL <="
-    cat $PAGE
+    if grep 'text\|empty' <(file -i $PAGE) >/dev/null
+    then
+        cat $PAGE
+        if [ $(tput lines) -lt $(wc -l < $PAGE) ]
+        then
+            less $PAGE
+        fi
+    else
+        file -i $PAGE
+        xdg-open $PAGE
+    fi
     unset LINK
     select LINK in . .. $(grep '^=>' $PAGE|cut -d' ' -f 2)
     do
